61037c52c0aea105ee0d7f674efac06cdd6e2ee3
Muna Mohammed Safrar <142378249+Muna-Mohammed-Safrar@users.noreply.github.com>
2024-01-06 19:20:57 +0400

    Add files via upload

---
+++ simple-1.lisp
@@ -0,0 +1,14 @@
+(write (+ 7 9 11))
+(terpri) ; Add newline
+
+(print (+ 2 3 1))
+(terpri) ; Add newline
+
+(write (+ (* (/ 9 5) 60) 32))
+(terpri) ; Add newline
+
+(write "Hello, world")
+(terpri) ; Add newline
+
+(format t "Sum: ~a~%" (+ 5 10))
+(terpri) ; Add newline
